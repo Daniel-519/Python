8b5b1ed224a9bddb869b6118cd843cdae1861ce0
LaremyWade <laremy.wade@outlook.com>
2020-03-02 18:16:42 -0600

    'initial'

---
+++ journal.py
@@ -0,0 +1,42 @@
+"""
+This is journal Module
+"""
+import os
+
+
+def load(name):
+    """
+    This method creates and load a new journals.
+
+    :param name: This base nae of the journal to load.
+    :return: A new journal data structure populated with the file data.
+    """
+
+    filename = get_full_url(name)
+    data = []
+
+    if (os.path.exists(filename)):
+        with open(filename) as fin:
+            for entry in fin.readlines():
+                data.append(entry.rstrip())
+    return data
+
+
+def save(name, journal_data):
+    filename = get_full_url(name)
+    print('..........saving to: {}'.format(filename))
+
+    with open(filename, 'w') as fout:
+        for entry in journal_data:
+            fout.write(entry + '\n')
+
+
+def get_full_url(name):
+    filename = os.path.abspath(os.path.join('./journals', name + '.jrl'))
+    return filename
+
+
+def add_entry(text, journal_data):
+    journal_data.append(text)
+
+
